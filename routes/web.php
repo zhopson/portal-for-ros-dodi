ab032dd87c310fef0831a45bfb835cf274da7f12
zhopson <man24@yandex.ru>
2017-09-06 15:59:23 +0900

    Добавлен web.php

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('lc', function () {
+  return redirect('/home');
+});
+
+Route::get('/telephony', 'TelephonyController@index')->name('telephony');
